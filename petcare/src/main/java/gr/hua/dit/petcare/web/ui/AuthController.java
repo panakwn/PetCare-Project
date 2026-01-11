038d49d93c343d6ca72d73ef11255ceb35b5e43d
manolisfen <manolis.sfen@gmail.com>
2026-01-11 17:39:52 +0200

    feat: Implement authentication check in login and register methods

--- petcare/src/main/java/gr/hua/dit/petcare/web/ui/AuthController.java
+++ petcare/src/main/java/gr/hua/dit/petcare/web/ui/AuthController.java
@@ -1,15 +1,19 @@
 package gr.hua.dit.petcare.web.ui;
 
-import gr.hua.dit.petcare.core.model.UserType;
-import gr.hua.dit.petcare.core.service.UserService;
-import gr.hua.dit.petcare.core.service.model.CreateUserRequest;
-import jakarta.validation.Valid;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import gr.hua.dit.petcare.core.model.UserType;
+import gr.hua.dit.petcare.core.service.UserService;
+import gr.hua.dit.petcare.core.service.model.CreateUserRequest;
+import jakarta.validation.Valid;
 
 @Controller
 public class AuthController {
@@ -20,21 +24,31 @@ public class AuthController {
         this.userService = userService;
     }
 
-    // --- LOGIN ---
+    
     @GetMapping("/login")
     public String login() {
+
+        if (isAuthenticated()) {
+            return "redirect:/";
+        }
         return "login";
     }
-    // --- REGISTER ---
+
+    
     @GetMapping("/register")
     public String register(Model model) {
+        
+        if (isAuthenticated()) {
+            return "redirect:/";
+        }
+
         CreateUserRequest emptyRequest = new CreateUserRequest(
-                "",             // username
-                "",             // email
-                "",             // password
-                "",             // firstName
-                "",             // lastName
-                UserType.PET_OWNER  // userType (default)
+                "",             
+                "",             
+                "",             
+                "",             
+                "",             
+                UserType.PET_OWNER  
         );
 
         model.addAttribute("user", emptyRequest);
@@ -58,5 +72,13 @@ public class AuthController {
         }
 
         return "redirect:/login";
+    }
+
+    private boolean isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || AnonymousAuthenticationToken.class.isAssignableFrom(authentication.getClass())) {
+            return false;
+        }
+        return authentication.isAuthenticated();
     }
 }
